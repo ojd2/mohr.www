525a7ffc3313f43bb75fbe42a23c5cf5e2a215ab
ojd2 <ojd2@st-andrews.ac.uk>
2018-04-06 12:44:04 +0100

    Updates to homepage layout

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -7,14 +7,42 @@ module.exports.homelist = function(req, res){
       strapline: 'Some Galleries!'
     },
     locations: [{
-      name: 'Lexo Gallery',
+      name: 'Cologne Lots',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      image:'/images/item-1.png',
+      size: 'medium',
+      facilities: ['Toilets', 'Food', 'Premium wifi'],
+      distance: '100m'
+    },
+    { 
+      name: 'Old Master & 19th Century Paintings and Drawings',
       address: '125 High Street, Reading, RG6 1PS',
       rating: 3,
       image:'/images/item-1.png',
       size: 'small',
       facilities: ['Toilets', 'Food', 'Premium wifi'],
       distance: '100m'
-    },{
+    },
+    { 
+      name: 'Jewellery & Boxes',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      image:'/images/item-1.png',
+      size: 'medium',
+      facilities: ['Toilets', 'Food', 'Premium wifi'],
+      distance: '100m'
+    },
+    { 
+      name: 'Kolodotschko Collection',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      image:'/images/item-1.png',
+      size: 'regular',
+      facilities: ['Toilets', 'Food', 'Premium wifi'],
+      distance: '100m'
+    },
+    {
       name: 'Shoreditch Times',
       address: '20 Shoreditch Street, Shoreditch, London, RG6 1PS',
       rating: 4,
@@ -22,8 +50,9 @@ module.exports.homelist = function(req, res){
       size: 'medium',
       facilities: ['Cafe', 'Food', 'Library'],
       distance: '221m'
-    },{
-      name: 'Roger Stone',
+    },
+    {
+      name: 'Tate Modern',
       address: '221 Lower Street, London, RG6 1PS',
       rating: 2,
       image:'/images/item-3.png',
@@ -39,20 +68,40 @@ module.exports.homelist = function(req, res){
       size: 'regular',
       facilities: ['Toilets', 'Food', 'Premium wifi'],
       distance: '100m'
-    },{
-      name: 'Shoreditch Times',
+    },
+    {
+      name: 'The Berlin Sale',
       address: '20 Shoreditch Street, Shoreditch, London, RG6 1PS',
       rating: 4,
       image:'/images/item-2.png',
       size: 'large',
       facilities: ['Cafe', 'Food', 'Library'],
       distance: '221m'
-    },{
-      name: 'Roger Stone',
+    },
+    {
+      name: '15th - 19th Century Paintings',
       address: '221 Lower Street, London, RG6 1PS',
       rating: 2,
       image:'/images/item-3.png',
       size: 'medium',
+      facilities: ['Cafe', 'Limited wifi'],
+      distance: '250m'
+    },
+    {
+      name: 'African & Oceanic Art Gallery',
+      address: '20 Shoreditch Street, Shoreditch, London, RG6 1PS',
+      rating: 4,
+      image:'/images/item-2.png',
+      size: 'regular',
+      facilities: ['Cafe', 'Food', 'Library'],
+      distance: '221m'
+    },
+    {
+      name: 'Netsuke IV',
+      address: '221 Lower Street, London, RG6 1PS',
+      rating: 2,
+      image:'/images/item-3.png',
+      size: 'large',
       facilities: ['Cafe', 'Limited wifi'],
       distance: '250m'
     }]
